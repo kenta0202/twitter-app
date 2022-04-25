fa762be161eb50f731bccb19e834e6a84b069d2a
kenta0202 <naiken0202@gmail.com>
2022-04-25 21:12:25 +0900

    Header Footer

--- src/components/general/Layout.tsx
+++ src/components/general/Layout.tsx
@@ -1,5 +1,5 @@
-import Header from "./header";
-import Footer from "./footer";
+import Header from "./Footer";
+import Footer from "./Header";
 
 export default function Layout({ children }) {
   return (
